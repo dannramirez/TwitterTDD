c306ce3300a15c7fcf889f17b070f8179d06bdbd
Daniel Ramirez <contact@dann.dev>
2022-04-26 19:57:44 -0500

    add: dateCreated and lastUpdated get current date and it's tests

--- test/User.test.js
+++ test/User.test.js
@@ -3,15 +3,15 @@ const User = require('../app/User')
 describe("Unit Tests for User class", () => {
 
     test('Create an User object', () => {
-    const user = new User(1,"dannramirez","Dann","Bio","dateCreated","lastUpdated");
+    const user = new User(1,"dannramirez","Dann","Bio");
     
     
-    expect(user.id).toBe(1)
-    expect(user.username).toBe("dannramirez")
-    expect(user.name).toBe("Dann")
-    expect(user.bio).toBe("Bio")
-    expect(user.dateCreated).toBe("dateCreated")
-    expect(user.lastUpdated).toBe("lastUpdated")
+    expect(user.id).toBe(1);
+    expect(user.userName).toBe("dannramirez");
+    expect(user.name).toBe("Dann");
+    expect(user.bio).toBe("Bio");
+    expect(user.dateCreated).not.toBeUndefined();
+    expect(user.lastUpdated).not.toBeUndefined();
 })
 
 })
