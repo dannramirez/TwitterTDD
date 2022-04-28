aeb7bec1b194ed133997b817daa105481753ea6d
Daniel Ramirez <contact@dann.dev>
2022-04-28 18:09:45 -0500

    fix: data send to constructor in test of UserService

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -1,7 +1,7 @@
 const User = require('./../User');
 class UserService {
     static create(id, username, name) {
-        return new User(id, username, name);
+        return new User(id, username, name, 'Sin bio');
     }
 }
 
--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -3,7 +3,7 @@
  describe('Test for UserService', () => { 
      test('1.Create a new user using the UserService', () => {
        const user = UserService.create(1, "dannramirez", "Dann");
-       expect(user.username).toBe("dannramirez");
+       expect(user.userName).toBe("dannramirez");
        expect(user.name).toBe("Dann");
        expect(user.id).toBe(1);
        expect(user.bio).not.toBeUndefined();
