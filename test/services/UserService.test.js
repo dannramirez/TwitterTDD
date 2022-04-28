295ac25c0c46906540b1a93784af8e6b7fcdaf67
Daniel Ramirez <contact@dann.dev>
2022-04-28 18:17:27 -0500

    add: Test 2 get all user data in a list

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -8,5 +8,14 @@
        expect(user.id).toBe(1);
        expect(user.bio).not.toBeUndefined();
      });
+
+     test("2. Get all user data in a list", () => {
+        const user = UserService.create(1, "dannramirez", "Dann");
+        const userInfoInList = UserService.getInfo(user);
+        expect(userInfoInList[0]).toBe(1);
+        expect(userInfoInList[1]).toBe("dannramirez");
+        expect(userInfoInList[2]).toBe("Dann");
+        expect (userInfoInList[3]). toBe("Sin bio");
+        });
      
   });
